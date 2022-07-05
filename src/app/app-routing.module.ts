605a11d6d538838f1de543c886fc871cc8e3bc5a
Bonarettim <78100234+Bonarettim@users.noreply.github.com>
2022-07-05 18:40:33 -0300

    add navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesModule } from './pages/categories/categories.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'categories',
+    loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
